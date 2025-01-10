eb04b2c154e70d6bd9f70351e1bd7fc190297a7e
Glenn Durfee <gdurfee@luckylooncottage.com>
2025-01-10 15:49:01 -0500

    Split the simulator into its own thread which communicates with the main thread via mpsc channels.

--- sim/src/main.rs
+++ sim/src/main.rs
@@ -4,11 +4,37 @@ mod params;
 mod rng;
 mod simulator;
 
+use std::io::stdin;
+use std::sync::mpsc::channel;
+use std::thread;
+
 use crate::params::GameParameters;
-use crate::simulator::Simulator;
+use crate::simulator::{Simulator, SimulatorInput, SimulatorOutput};
 
 fn main() {
     let (seed, character, ascension) = GameParameters::from_command_line();
-    let simulator = Simulator::new(seed, character, ascension);
-    simulator.run();
+    let (input_tx, input_rx) = channel();
+    let (output_tx, output_rx) = channel();
+    let simulator = Simulator::new(seed, character, ascension, input_rx, output_tx);
+    thread::spawn(move || {
+        simulator.run();
+    });
+    loop {
+        match output_rx.recv() {
+            Ok(SimulatorOutput::Map(map)) => println!("{}", map),
+            Ok(SimulatorOutput::AwaitingInput(state)) => {
+                println!("{:?}", state);
+                let mut user_input = String::new();
+                match stdin().read_line(&mut user_input) {
+                    Ok(_) => {
+                        if let Err(e) = input_tx.send(user_input) {
+                            panic!("Error sending input: {}", e);
+                        }
+                    }
+                    Err(e) => panic!("Error reading input: {}", e),
+                }
+            }
+            Err(e) => panic!("Error receiving output: {}", e),
+        }
+    }
 }
--- sim/src/simulator.rs
+++ sim/src/simulator.rs
@@ -1,56 +1,88 @@
-use std::io::stdin;
+use std::sync::mpsc::{Receiver, Sender};
 
 use crate::data::{Act, Ascension, Character, Event};
 use crate::map::{MapBuilder, NodeGrid};
 use crate::rng::{EncounterGenerator, Seed};
 
+// TODO: Use structured input
+pub type SimulatorInput = String;
+
+/// The output of the simulator. This is sent to the main thread for display and to await user
+/// input, or to an AI for training and evaluation.
+#[derive(Clone, Debug)]
+pub enum SimulatorOutput {
+    // ASCII representation of the current map
+    Map(String),
+    // TODO: Limit output to a subset of the internal state
+    AwaitingInput(InternalState),
+}
+
 pub struct Simulator {
     seed: Seed,
     character: Character,
     ascension: Ascension,
+    input_rx: Receiver<SimulatorInput>,
+    output_tx: Sender<SimulatorOutput>,
+    state: InternalState,
     map: NodeGrid,
     encounter_generator: EncounterGenerator,
-    state: State,
 }
 
-#[derive(Debug)]
-enum State {
+// TODO: restrict visibility
+#[derive(Clone, Copy, Debug)]
+pub enum InternalState {
     Event(Event),
 }
 
 impl Simulator {
-    pub fn new(seed: Seed, character: Character, ascension: Ascension) -> Self {
+    pub fn new(
+        seed: Seed,
+        character: Character,
+        ascension: Ascension,
+        input_rx: Receiver<String>,
+        output_tx: Sender<SimulatorOutput>,
+    ) -> Self {
         let map = MapBuilder::from(&seed, ascension, Act::get(1)).build();
         let encounter_generator = EncounterGenerator::new(&seed);
-        let state = State::Event(Event::Neow);
+        let state = InternalState::Event(Event::Neow);
         Self {
             seed,
             character,
             ascension,
+            input_rx,
+            output_tx,
+            state,
             map,
             encounter_generator,
-            state,
         }
     }
 
     pub fn run(mut self) {
-        let mut user_input = String::new();
+        println!("Simulator started");
+        if let Err(e) = self
+            .output_tx
+            .send(SimulatorOutput::Map(self.map.to_string()))
+        {
+            println!("Error initial sending map: {}", e);
+            return;
+        }
         loop {
-            self.display_state();
-            match stdin().read_line(&mut user_input) {
-                Ok(0) | Err(_) => {
+            if let Err(e) = self
+                .output_tx
+                .send(SimulatorOutput::AwaitingInput(self.state))
+            {
+                println!("Error sending state: {}", e);
+                break;
+            }
+            match self.input_rx.recv() {
+                Ok(input) => self.evolve_state(input.trim()),
+                Err(e) => {
+                    println!("Error receiving input: {}", e);
                     break;
                 }
-                Ok(_) => {
-                    self.evolve_state(user_input.trim());
-                }
             }
-            user_input.clear();
         }
-    }
-
-    fn display_state(&self) {
-        println!("Current state: {:?}", self.state);
+        println!("Simulator finished");
     }
 
     fn evolve_state(&mut self, user_input: &str) {

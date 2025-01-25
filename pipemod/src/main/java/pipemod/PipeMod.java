6581ec8d9c488b036f04a8d351c4a219306885c8
Glenn Durfee <gdurfee@luckylooncottage.com>
2025-01-25 18:58:04 -0500

    Removing some unneeded files left over from the tutorial code

--- pipemod/src/main/java/pipemod/PipeMod.java
+++ pipemod/src/main/java/pipemod/PipeMod.java
@@ -2,35 +2,25 @@ package pipemod;
 
 import basemod.BaseMod;
 import basemod.interfaces.PostInitializeSubscriber;
-import com.badlogic.gdx.Files;
-import com.badlogic.gdx.backends.lwjgl.LwjglFileHandle;
-import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.graphics.Texture;
 import com.evacipated.cardcrawl.modthespire.Loader;
 import com.evacipated.cardcrawl.modthespire.ModInfo;
 import com.evacipated.cardcrawl.modthespire.Patcher;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
-import com.megacrit.cardcrawl.core.Settings;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.scannotation.AnnotationDB;
-import pipemod.util.GeneralUtils;
-import pipemod.util.KeywordInfo;
-import pipemod.util.TextureLoader;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 @SpireInitializer
-public class PipeMod implements
-        PostInitializeSubscriber {
+public class PipeMod implements PostInitializeSubscriber {
     public static ModInfo info;
     public static String modID; // Edit your pom.xml to change this
     static {
         loadModInfo();
     }
-    private static final String resourcesFolder = checkResourcesPath();
     public static final Logger logger = LogManager.getLogger(modID); // Used to output to the console.
 
     // This is used to prefix the IDs of various objects like cards and relics,
@@ -53,20 +43,9 @@ public class PipeMod implements
 
     @Override
     public void receivePostInitialize() {
-        // This loads the image used as an icon in the in-game mods menu.
-        Texture badgeTexture = TextureLoader.getTexture(imagePath("badge.png"));
-        // Set up the mod information displayed in the in-game mods menu.
-        // The information used is taken from your pom.xml file.
-
         logger.info(modID + " Hello, world.");
         connectPipe();
         logger.info(modID + " Hello, world again.");
-
-        // If you want to set up a config panel, that will be done here.
-        // You can find information about this on the BaseMod wiki page "Mod Config and
-        // Panel".
-        BaseMod.registerModBadge(badgeTexture, info.Name, GeneralUtils.arrToString(info.Authors), info.Description,
-                null);
     }
 
     public void connectPipe() {
@@ -94,70 +73,6 @@ public class PipeMod implements
     }
 
     /*----------Localization----------*/
-
-    // This is used to load the appropriate localization files based on language.
-    private static String getLangString() {
-        return Settings.language.name().toLowerCase();
-    }
-
-    private static final String defaultLanguage = "eng";
-
-    public static final Map<String, KeywordInfo> keywords = new HashMap<>();
-
-    // These methods are used to generate the correct filepaths to various parts of
-    // the resources folder.
-    public static String localizationPath(String lang, String file) {
-        return resourcesFolder + "/localization/" + lang + "/" + file;
-    }
-
-    public static String imagePath(String file) {
-        return resourcesFolder + "/images/" + file;
-    }
-
-    public static String characterPath(String file) {
-        return resourcesFolder + "/images/character/" + file;
-    }
-
-    public static String powerPath(String file) {
-        return resourcesFolder + "/images/powers/" + file;
-    }
-
-    public static String relicPath(String file) {
-        return resourcesFolder + "/images/relics/" + file;
-    }
-
-    /**
-     * Checks the expected resources path based on the package name.
-     */
-    private static String checkResourcesPath() {
-        String name = PipeMod.class.getName(); // getPackage can be iffy with patching, so class name is used instead.
-        int separator = name.indexOf('.');
-        if (separator > 0)
-            name = name.substring(0, separator);
-
-        FileHandle resources = new LwjglFileHandle(name, Files.FileType.Internal);
-
-        if (!resources.exists()) {
-            throw new RuntimeException("\n\tFailed to find resources folder; expected it to be named \"" + name + "\"."
-                    +
-                    " Either make sure the folder under resources has the same name as your mod's package, or change the line\n"
-                    +
-                    "\t\"private static final String resourcesFolder = checkResourcesPath();\"\n" +
-                    "\tat the top of the " + PipeMod.class.getSimpleName() + " java file.");
-        }
-        if (!resources.child("images").exists()) {
-            throw new RuntimeException("\n\tFailed to find the 'images' folder in the mod's 'resources/" + name
-                    + "' folder; Make sure the " +
-                    "images folder is in the correct location.");
-        }
-        if (!resources.child("localization").exists()) {
-            throw new RuntimeException("\n\tFailed to find the 'localization' folder in the mod's 'resources/" + name
-                    + "' folder; Make sure the " +
-                    "localization folder is in the correct location.");
-        }
-
-        return name;
-    }
 
     /**
      * This determines the mod's ID based on information stored by ModTheSpire.
